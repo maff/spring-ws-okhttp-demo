66c914d7c148d566d81199939e2f8c2744ccd45f
Mathias Geat <mathias@ailoo.net>
2023-01-06 13:41:21 +0100

    Initial import

---
+++ src/main/java/com/example/springwsokhttpdemo/SpringWsOkhttpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwsokhttpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsOkhttpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWsOkhttpDemoApplication.class, args);
+    }
+
+}
